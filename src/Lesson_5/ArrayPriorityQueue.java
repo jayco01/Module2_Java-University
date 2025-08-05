1adf2d4e97019c2adfad657ea47cfabb86f4178f
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-05 12:48:10 -0600

    Feat: Initialized Lesson 5 Task

---
+++ src/Lesson_5/ArrayPriorityQueue.java
@@ -0,0 +1,22 @@
+package Lesson_5;
+
+public class ArrayPriorityQueue<T extends HasPriority> implements PriorityQueue<T> {
+    // TODO: Implement Array Prio Queue using an array as the data storage
+    // As we have discussed and drew
+    T[] data;
+
+    @Override
+    public void put(T elem) {
+        // elem.getPriority()
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
