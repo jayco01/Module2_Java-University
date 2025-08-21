cb02f1109c17e0ed2afd87d33d5b1dd49af7a81c
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-21 13:21:25 -0600

    Feat: countCharOccurrences is fully functional

--- src/Lesson_10/Main.java
+++ src/Lesson_10/Main.java
@@ -2,8 +2,20 @@ package Lesson_10;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nubmers = {1,2,3,6,5,4};
+        int[] numbers = {1,2,3,6,5,4};
+        String str = "I really like eating Italian sandwich for lunch.";
+        char charToCount = 'i';
+        
+        System.out.print("The Max Number in the Array is: ");
+        System.out.println(Recursion.findMaxNumInArray(numbers));
 
-        System.out.println(Recursion.findMaxNumInArray(nubmers));
+        System.out.print("The Sum of all the elements in the Array is: ");
+        System.out.println(Recursion.sumAllElementsInArray(numbers));
+        
+        System.out.print("The number of '" + charToCount + "' elements in the String is: ");
+        System.out.println(Recursion.countCharOccurrences(str, charToCount));
+
+        System.out.print("The number of '" + charToCount + "' elements in the String is: ");
+        System.out.println(Recursion.countCharOccurrences(str, charToCount));
     }
 }
--- src/Lesson_10/Recursion.java
+++ src/Lesson_10/Recursion.java
@@ -2,7 +2,40 @@ package Lesson_10;
 
 import java.util.Arrays;
 
+// Count the number of occurrence of a char in a string
 public class Recursion {
+    public static int countCharOccurrences(String str, char ch) {
+        int count = 0;
+        int index = 0;
+
+        return recursiveCountCharOccurrences(ch, count, str, index);
+    }
+
+    private static int recursiveCountCharOccurrences(char ch, int count, String str, int index) {
+        if (index == str.length()) {return count;}
+        char lowerCh = Character.toLowerCase(ch);
+        char lowerStrCh = Character.toLowerCase(str.charAt(index));
+        if(Character.compare(lowerCh, lowerStrCh) == 0) count++;
+
+        return recursiveCountCharOccurrences(ch, count, str, index+1);
+    }
+
+    // Sum all elements in Array
+    public static int sumAllElementsInArray(int[] arr) {
+        int sum = 0;
+        int index = 0;
+
+        return recursiveSumAllElementsInArray(sum, arr, index);
+    }
+
+    private static int recursiveSumAllElementsInArray(int sum, int[] arr, int index) {
+        if (index == arr.length) {return sum;}
+        sum += arr[index];
+
+        return recursiveSumAllElementsInArray(sum, arr, index + 1);
+    }
+
+    // Find Max Number in Array
     public static int findMaxNumInArray(int[] arr) {
         int maxNum = Integer.MIN_VALUE;
         int index = 0;
